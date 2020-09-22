9bc3310a1366d0813c42b6e8cd5eb97d6da37356
Leonardo de Melo Alves <leunardosevla@gmail.com>
2020-09-21 23:02:28 -0300

    feat: add new custom query for client

--- client-service/src/main/java/com/ifood/demo/client/ClientRepository.java
+++ client-service/src/main/java/com/ifood/demo/client/ClientRepository.java
@@ -3,6 +3,7 @@ package com.ifood.demo.client;
 import java.util.Collection;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -18,4 +19,17 @@ public interface ClientRepository extends CrudRepository<Client, UUID> {
 	
 	@RestResource(path = "byEmail")
 	Collection<Client> findByEmailIgnoreCaseContaining(@Param("email") String email);
+
+	@RestResource
+	@Query(
+		"SELECT c FROM Client c WHERE "
+		+ "(:name is null or c.name like %:name%) and"
+		+ "(:phone is null or c.phone like %:phone%) and"
+		+ "(:email is null or c.email like %:email%)"
+	)
+	Collection<Client> findAllClients(
+		@Param("name") String name,
+		@Param("phone") String phone,
+		@Param("email") String email
+	);
 }
