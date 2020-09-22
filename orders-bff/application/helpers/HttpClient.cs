250afb183cf072edb565062dbda046284c983f11
Leonardo de Melo Alves <leunardosevla@gmail.com>
2020-09-22 17:24:33 -0300

    feat: add http client helper

---
+++ orders-bff/application/helpers/HttpClient.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Newtonsoft.Json;
+
+namespace application.helpers
+{
+    public class HttpClient : System.Net.Http.HttpClient
+    {
+        private string _queryParams;
+        private string _resourcePath;
+        public HttpClient() : base()
+        {
+        }
+
+        public async Task<T> GetAsync<T>()
+        {
+            var request = await base.GetStringAsync(_resourcePath + GetQueryParameters());
+            return  JsonConvert.DeserializeObject<T>(request);
+        }
+
+
+        public HttpClient WithUrl(string resourcePath)
+        {
+            _resourcePath = resourcePath;
+            return this;
+        }
+
+        public HttpClient AddQueryParameter(string key, object value)
+        {
+            if (value != null)
+            {
+                _queryParams += $"&{key}={value.ToString()}";
+            }
+
+            return this;
+        }
+
+        private string GetQueryParameters() => "?" + _queryParams.Substring(1);
+    }
+}
