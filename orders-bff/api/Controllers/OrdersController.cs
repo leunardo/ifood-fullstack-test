021421b5eb8d673947bb6a248df544e962a1264e
Leonardo de Melo Alves <leunardosevla@gmail.com>
2020-09-22 21:51:17 -0300

    feat: add swagger documentation

--- orders-bff/api/Controllers/OrdersController.cs
+++ orders-bff/api/Controllers/OrdersController.cs
@@ -15,7 +15,6 @@ namespace api.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly ILogger<OrdersController> _logger;
-        private readonly IClientService _clientService;
         private readonly IOrderService _orderService;
 
         public OrdersController(
@@ -25,10 +24,17 @@ namespace api.Controllers
         )
         {
             _logger = logger;
-            _clientService = clientService;
             _orderService = orderService;
         }
 
+        /// <summary>
+        /// Searches for orders along with resumed information about the client.
+        /// </summary>
+        /// <param name="name">The Client Name</param> 
+        /// <param name="phone">The Client's Phone number</param> 
+        /// <param name="email">The Client's E-mail</param> 
+        /// <param name="startDate">The start date for the Creation Date of the Order</param> 
+        /// <param name="endDate">The end date for the Creation Date of the Order</param> 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<OrderDto>>> Search(
             [FromQuery] String name,
@@ -53,9 +59,15 @@ namespace api.Controllers
             return Ok(await _orderService.Search(dto));
         }
 
+        /// <summary>
+        /// Gets an Order with all the details about items and the client.
+        /// </summary>
+        /// <param name="id">The ID of the order</param> 
         [HttpGet("{id}")]
         public async Task<ActionResult<OrderFullDto>> GetFullOrder([FromRoute] Guid id)
         {
+            _logger.Log(LogLevel.Information, "Get Full Order", id);
+
             return Ok(await _orderService.GetOrder(id));
         }
     }
--- orders-bff/api/Startup.cs
+++ orders-bff/api/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using application.helpers;
 using application.interfaces;
@@ -15,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace api
 {
@@ -43,6 +46,27 @@ namespace api
             });
             IMapper mapper = config.CreateMapper();
             services.AddSingleton(mapper);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "IFood BFF Orders API",
+                    Description = "A simple Back-end For Front-end api to consume orders for IFood",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Leonardo Alves",
+                        Email = "leo@leonardoalves.dev",
+                        Url = new Uri("https://leonardoalves.dev"),
+                    }
+                });
+                
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +76,17 @@ namespace api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "IFood Back-end for Front-end Orders API v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
