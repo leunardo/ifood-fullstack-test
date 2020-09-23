8b64c17c434386fb66b9277d7bf3f164974f5460
Leonardo de Melo Alves <leunardosevla@gmail.com>
2020-09-22 23:06:44 -0300

    feat: add post async in http helper

--- orders-bff/application/helpers/HttpClient.cs
+++ orders-bff/application/helpers/HttpClient.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Newtonsoft.Json;
@@ -31,16 +32,30 @@ namespace application.helpers
 
         public class HttpClientExecutable : HttpClient
         {
-            public HttpClientExecutable(string queryParams, string resourcePath)
+            public HttpClientExecutable(string queryParams, string resourcePath) : base()
             {
                 base._queryParams = queryParams;
                 base._resourcePath = resourcePath;
+                this.DefaultRequestHeaders.Add("Accept", "application/json");
             }
 
             public async Task<T> GetAsync<T>()
             {
                 var request = await base.GetStringAsync(_resourcePath + GetQueryParameters());
                 return  JsonConvert.DeserializeObject<T>(request);
+            }
+
+            public async Task<T> PostAsync<T, U>(U body)
+            {
+                var payload = JsonConvert.SerializeObject(body);
+                var request = await base.PostAsync(
+                    _resourcePath + GetQueryParameters(),
+                    new StringContent(payload, Encoding.UTF8, "application/json")
+                );
+
+                var response = await request.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<T>(response);
             }
 
             private string GetQueryParameters() => string.IsNullOrEmpty(_queryParams)
