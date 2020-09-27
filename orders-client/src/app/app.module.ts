aa15ca0040ec0de5cef708157799d5be5b6f234f
Leonardo de Melo Alves <leunardosevla@gmail.com>
2020-09-26 21:42:16 -0300

    feat: add angular material

--- orders-client/src/app/app.module.ts
+++ orders-client/src/app/app.module.ts
@@ -2,18 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { OrdersTableComponent } from './components/orders-table/orders-table.component';
+import { OrdersSearchComponent } from './components/orders-search/orders-search.component';
+import { OrdersDetailsComponent } from './components/orders-details/orders-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersTableComponent,
+    OrdersSearchComponent,
+    OrdersDetailsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
